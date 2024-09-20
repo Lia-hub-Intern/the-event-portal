bd6ac8bd2e8ac9d7cae393e204f98c26c8c98da0
Andy <andycheung83a@hotmail.com>
2024-09-20 12:40:05 +0200

    Refactor Speakers component to add Events view and improve code structure

--- frontend/src/components/views/Speakers.jsx
+++ frontend/src/components/views/Speakers.jsx
@@ -3,8 +3,9 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { top100Films } from '../functions/Functions';
+import Grid from '@mui/material/Grid2';
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -13,32 +14,38 @@ export default function Speakers() {
 	return (
 		<>
 			<Box>
-				<h1>Events</h1>
-				<Autocomplete
-					multiple
-					id="checkboxes-tags-demo"
-					options={top100Films}
-					disableCloseOnSelect
-					getOptionLabel={(option) => option.title}
-					renderOption={(props, option, { selected }) => {
-						const { key, ...optionProps } = props;
-						return (
-							<li key={key} {...optionProps}>
-								<Checkbox
-									icon={icon}
-									checkedIcon={checkedIcon}
-									style={{ marginRight: 8 }}
-									checked={selected}
-								/>
-								{option.title}
-							</li>
-						);
-					}}
-					style={{ width: 500 }}
-					renderInput={(params) => (
-						<TextField {...params} label="Checkboxes" placeholder="Favorites" />
-					)}
-				/>
+				<Grid>
+					<Typography variant="h3">Events</Typography>
+					<Autocomplete
+						multiple
+						id="checkboxes-tags-demo"
+						options={top100Films}
+						disableCloseOnSelect
+						getOptionLabel={(option) => option.title}
+						renderOption={(props, option, { selected }) => {
+							const { key, ...optionProps } = props;
+							return (
+								<li key={key} {...optionProps}>
+									<Checkbox
+										icon={icon}
+										checkedIcon={checkedIcon}
+										style={{ marginRight: 8 }}
+										checked={selected}
+									/>
+									{option.title}
+								</li>
+							);
+						}}
+						style={{ width: 500 }}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								label="Checkboxes"
+								placeholder="Favorites"
+							/>
+						)}
+					/>
+				</Grid>
 			</Box>
 		</>
 	);
