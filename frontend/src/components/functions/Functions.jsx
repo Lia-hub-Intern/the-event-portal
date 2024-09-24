5c71baf422f9a67613e585c1f3667cd3d15b7c9d
Andy <andycheung83a@hotmail.com>
2024-09-24 18:06:05 +0200

    Update typography styles in Themes.jsx and add Autocomplete style overrides
    - Update typography styles for p1, p2, h5, and h6A in Themes.jsx
    - Add style overrides for MuiAutocomplete inputRoot in Themes.jsx

--- frontend/src/components/functions/Functions.jsx
+++ frontend/src/components/functions/Functions.jsx
@@ -22,106 +22,166 @@ export const navBarLinks = [
 ];
 
 export const logos = [
-	{
-		title: 'Logo 1',
-		image:
-			'https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=626&ext=jpg;',
-		description:
-			'For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovation.',
-	},
-	{
-		title: 'Logo 2',
-		image:
-			'https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg',
-		description:
-			'For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.',
-	},
-	{
-		title: 'Logo 3',
-		image:
-			'https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg;',
-		description:
-			'For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.',
-	},
+  {
+    name: "Karl Lillrud",
+    title: "Professional Keynote Speaker & Business Coach",
+    image:
+      "https://static.wixstatic.com/media/4d53c3_40db86ddf78f4270961713c4c7a3e645~mv2_d_1617_2048_s_2.jpg/v1/crop/x_0,y_44,w_1595,h_1956/fill/w_548,h_672,al_c,q_90,enc_auto/Karl%20Lillrud%20.jpg",
+    description:
+      "Karl Lillrud makes sure you get not just advice but also the tools to apply straight away to make a difference and improve your Future, AI knowledge, Innovation thinking, and Leadership mindset.",
+  },
+  {
+    name: "Speaker 2",
+    title: "Logo 2",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 3",
+    title: "Logo 3",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 4",
+    title: "Logo 4",
+    image:
+      "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovation.",
+  },
+  {
+    name: "Speaker 5",
+    title: "Logo 5",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 6",
+    title: "Logo 6",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 7",
+    title: "Logo 7",
+    image:
+      "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovation.",
+  },
+  {
+    name: "Speaker 8",
+    title: "Logo 8",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 9",
+    title: "Logo 9",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
+  {
+    name: "Speaker 10",
+    title: "Logo 10",
+    image:
+      "https://img.freepik.com/free-vector/bicycle-shop-logo-design-vector_53876-40626.jpg?size=626&ext=jpg",
+    description:
+      "For the very first time, Europe is hosting a TED event to celebrate groundbreaking ideas at the forefront of AI innovationxxxxx.",
+  },
 ];
+
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 export const top100Films = [
-	{ title: 'The Shawshank Redemption', year: 1994 },
-	{ title: 'The Godfather', year: 1972 },
-	{ title: 'The Godfather: Part II', year: 1974 },
-	{ title: 'The Dark Knight', year: 2008 },
-	{ title: '12 Angry Men', year: 1957 },
-	{ title: "Schindler's List", year: 1993 },
-	{ title: 'Pulp Fiction', year: 1994 },
-	{
-		title: 'The Lord of the Rings: The Return of the King',
-		year: 2003,
-	},
-	{ title: 'The Good, the Bad and the Ugly', year: 1966 },
-	{ title: 'Fight Club', year: 1999 },
-	{
-		title: 'The Lord of the Rings: The Fellowship of the Ring',
-		year: 2001,
-	},
-	{
-		title: 'Star Wars: Episode V - The Empire Strikes Back',
-		year: 1980,
-	},
-	{ title: 'Forrest Gump', year: 1994 },
-	{ title: 'Inception', year: 2010 },
-	{
-		title: 'The Lord of the Rings: The Two Towers',
-		year: 2002,
-	},
-	{ title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-	{ title: 'Goodfellas', year: 1990 },
-	{ title: 'The Matrix', year: 1999 },
-	{ title: 'Seven Samurai', year: 1954 },
-	{
-		title: 'Star Wars: Episode IV - A New Hope',
-		year: 1977,
-	},
-	{ title: 'City of God', year: 2002 },
-	{ title: 'Se7en', year: 1995 },
-	{ title: 'The Silence of the Lambs', year: 1991 },
-	{ title: "It's a Wonderful Life", year: 1946 },
-	{ title: 'Life Is Beautiful', year: 1997 },
-	{ title: 'The Usual Suspects', year: 1995 },
-	{ title: 'Léon: The Professional', year: 1994 },
-	{ title: 'Spirited Away', year: 2001 },
-	{ title: 'Saving Private Ryan', year: 1998 },
-	{ title: 'Once Upon a Time in the West', year: 1968 },
-	{ title: 'American History X', year: 1998 },
-	{ title: 'Interstellar', year: 2014 },
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
+  { title: "The Good, the Bad and the Ugly", year: 1966 },
+  { title: "Fight Club", year: 1999 },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001,
+  },
+  {
+    title: "Star Wars: Episode V - The Empire Strikes Back",
+    year: 1980,
+  },
+  { title: "Forrest Gump", year: 1994 },
+  { title: "Inception", year: 2010 },
+  {
+    title: "The Lord of the Rings: The Two Towers",
+    year: 2002,
+  },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: "Goodfellas", year: 1990 },
+  { title: "The Matrix", year: 1999 },
+  { title: "Seven Samurai", year: 1954 },
+  {
+    title: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+  },
+  { title: "City of God", year: 2002 },
+  { title: "Se7en", year: 1995 },
+  { title: "The Silence of the Lambs", year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: "Life Is Beautiful", year: 1997 },
+  { title: "The Usual Suspects", year: 1995 },
+  { title: "Léon: The Professional", year: 1994 },
+  { title: "Spirited Away", year: 2001 },
+  { title: "Saving Private Ryan", year: 1998 },
+  { title: "Once Upon a Time in the West", year: 1968 },
+  { title: "American History X", year: 1998 },
+  { title: "Interstellar", year: 2014 },
 ];
 
 export async function textToArray(data) {
-	const lines = data.split('\n');
-	const events = [];
+  const lines = data.split("\n");
+  const events = [];
 
-	let currentEvent = {};
-	let description = '';
+  let currentEvent = {};
+  let description = "";
 
-	for (const line of lines) {
-		if (line.startsWith('**')) {
-			if (Object.keys(currentEvent).length > 0) {
-				events.push(currentEvent);
-			}
-			currentEvent = {};
-			currentEvent.title = line.substring(2, line.length - 2);
-			description = '';
-		} else if (line.includes(':')) {
-			const [key, values] = line.split(':');
-			currentEvent[key.trim()] = values.trim();
-		} else {
-			description += line + ' ';
-		}
-	}
+  for (const line of lines) {
+    if (line.startsWith("**")) {
+      if (Object.keys(currentEvent).length > 0) {
+        events.push(currentEvent);
+      }
+      currentEvent = {};
+      currentEvent.title = line.substring(2, line.length - 2);
+      description = "";
+    } else if (line.includes(":")) {
+      const [key, values] = line.split(":");
+      currentEvent[key.trim()] = values.trim();
+    } else {
+      description += line + " ";
+    }
+  }
 
-	if (Object.keys(currentEvent).length > 0) {
-		currentEvent.description = description.trim();
-		events.push(currentEvent);
-	}
+  if (Object.keys(currentEvent).length > 0) {
+    currentEvent.description = description.trim();
+    events.push(currentEvent);
+  }
 
-	return events;
+  return events;
 }
--- frontend/src/components/views/Speakers.jsx
+++ frontend/src/components/views/Speakers.jsx
@@ -7,7 +7,6 @@ import Autocomplete from '@mui/material/Autocomplete';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { top100Films, logos } from '../functions/Functions';
-
 import {
 	Box,
 	Typography,
@@ -24,160 +23,81 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />;
 export default function Speakres() {
 	return (
 		<>
-			{/*<Box>
-        <Grid container>
-          <Typography variant="h3">
-            Events
-            <Typography />
-          </Typography>
-
-          <Autocomplete
-            multiple
-            id="checkboxes-tags-demo"
-            options={top100Films}
-            disableCloseOnSelect
-            getOptionLabel={(option) => option.title}
-            renderOption={(props, option, { selected }) => {
-              const { key, ...optionProps } = props;
-              return (
-                <li key={key} {...optionProps}>
-                  <Checkbox
-                    icon={icon}
-                    checkedIcon={checkedIcon}
-                    style={{ marginRight: 8 }}
-                    checked={selected}
-                  />
-                  {option.title}
-                </li>
-              );
-            }}
-            style={{ width: 500 }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Checkboxes"
-                placeholder="Favorites"
-              />
-            )}
-          />
-        </Grid>
-        <Grid>
-          {logos.map((item) => (
-            <Card
-              key={item.logo}
-              sx={{
-                marginLeft: { xs: 2, sm: 12 },
-                marginTop: 2,
-                boxShadow: "none",
-                textDecoration: "none",
-                display: { xs: "block", sm: "flex" },
-                height: { xs: "36rem", sm: "20rem" },
-              }}
-            >
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <CardMedia
-                  component="img"
-                  image={item.logo}
-                  alt="Card Image"
-                  sx={{
-                    paddingTop: 1,
-                    width: { xs: "15rem", sm: "18rem" },
-                    marginLeft: { xs: "3rem", sm: "0rem" },
-                  }}
-                />
-              </Box>
-
-              <Box
-                sx={{
-                  display: { xs: "block", sm: "flex" },
-                  //display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardContent
-                  sx={{
-                    justifyContent: "right",
-                    justifyItems: "right",
-                  }}
-                >
-                  <Typography
-                    component="span"
-                    sx={{
-                      fontSize: 40,
-                      fontWeight: 400,
-                      lineHeight: "2rem",
-                    }}
-                  >
-                    {item.tite}
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
-          ))}
-        </Grid>
-      </Box>*/}
-
 			<Grid
 				container
-				spacing={20}
+				spacing={10}
 				rowSpacing={1}
 				columnSpacing={{ xs: 1, sm: 2, md: 3 }}
 				sx={{
-					marginLeft: 20,
 					justifyContent: 'center',
 					justifyItems: 'center',
-					width: '200vh',
-					height: '160vh',
 					display: { xs: 'block', sm: 'flex' },
 				}}
 			>
-				<Box>
-					<Typography variant="h3">
-						Events
-						<Typography />
-					</Typography>
-
-					<Autocomplete
-						multiple
-						id="checkboxes-tags-demo"
-						options={top100Films}
-						disableCloseOnSelect
-						getOptionLabel={(option) => option.title}
-						renderOption={(props, option, { selected }) => {
-							const { key, ...optionProps } = props;
-							return (
-								<li key={key} {...optionProps}>
-									<Checkbox
-										icon={icon}
-										checkedIcon={checkedIcon}
-										style={{ marginRight: 8 }}
-										checked={selected}
-									/>
-									{option.title}
-								</li>
-							);
-						}}
-						style={{ width: 500 }}
-						renderInput={(params) => (
-							<TextField
-								{...params}
-								label="Checkboxes"
-								placeholder="Favorites"
-							/>
-						)}
-					/>
-				</Box>
 				<Grid
-					spacing={20}
-					rowSpacing={1}
-					columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+					container //style={{ minHeight: "80vh" }}
 					sx={{
-						marginLeft: 20,
+						item: { xs: 12, sm: 6, md: 4 },
 						justifyContent: 'center',
 						justifyItems: 'center',
-						width: '200vh',
-						height: '160vh',
-						display: { xs: 'block', sm: 'flex' },
+					}}
+				>
+					<Box
+						sx={{
+							width: { xs: '20rem', sm: 'auto' },
+							marginBottom: '1rem ',
+						}}
+					>
+						<Typography variant="h4">
+							Speakers list
+							<Typography />
+						</Typography>
+
+						<Autocomplete
+							multiple
+							id="checkboxes-tags-demo"
+							options={top100Films}
+							disableCloseOnSelect
+							getOptionLabel={(option) => option.title}
+							renderOption={(props, option, { selected }) => {
+								const { key, ...optionProps } = props;
+								return (
+									<li key={key} {...optionProps}>
+										<Checkbox
+											icon={icon}
+											checkedIcon={checkedIcon}
+											style={{ marginRight: 8 }}
+											checked={selected}
+										/>
+										{option.title}
+									</li>
+								);
+							}}
+							//style={{ width: 500 }}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									label="Checkboxes"
+									placeholder="Favorites"
+								/>
+							)}
+						/>
+					</Box>
+				</Grid>
+
+				<Grid
+					container
+					spacing={10}
+					rowSpacing={1}
+					columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+					//style={{ minHeight: "80vh" }}
+					sx={{
+						item: { xs: 12, sm: 6, md: 4 },
+						justifyContent: 'center',
+						justifyItems: 'center',
+						//width: "200vh",
+						//height: "160vh",
+						//style: { minHeight: "160vh" },
 					}}
 				>
 					{logos.map((item) => (
@@ -192,10 +112,10 @@ export default function Speakres() {
 								'&:hover': {
 									transform: 'scale(1.05)',
 								},
-								width: '60vh',
-								height: '70vh',
-								marginLeft: 2,
-								marginTop: 2,
+								width: { xs: '38vh', sm: '40vh' },
+								height: { xs: '72vh', sm: '95vh' },
+								marginLeft: { sm: '1rem' },
+								marginTop: '2rem',
 								textDecoration: 'none',
 							}}
 						>
@@ -206,27 +126,30 @@ export default function Speakres() {
 									component="img"
 									image={item.image}
 									alt="Card Image"
-									sx={{ height: '40vh' }}
+									sx={{ height: '48vh' }}
 								/>
 
-								{/** Encierra todo el contenido de texto */}
+								{/** Encloses all text content */}
 								<CardContent
 									sx={{
-										height: '7rem',
+										height: '9rem',
 										justifyContent: 'left',
 										justifyItems: 'left',
 									}}
 								>
 									<Typography
-										component="h2"
-										sx={{ textTransform: 'capitalize' }}
+										variant="h5"
+										sx={{
+											paddingBottom: '1rem',
+											textTransform: 'capitalize',
+										}}
 									>
+										{item.name}
+									</Typography>
+									<Typography variant="p1" component="p">
 										{item.title}
 									</Typography>
-									<Typography
-										component="p"
-										sx={{ fontSize: 12, fontWeight: 300 }}
-									>
+									<Typography variant="p2" component="">
 										{item.description}
 									</Typography>
 								</CardContent>
