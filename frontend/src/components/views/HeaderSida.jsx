51baacae819fe85130847b3d6d21877632927a09
Andy <andycheung83a@hotmail.com>
2024-10-04 14:26:03 +0200

    Add responsive image grid to HeaderSida component

--- frontend/src/components/views/HeaderSida.jsx
+++ frontend/src/components/views/HeaderSida.jsx
@@ -1,10 +1,37 @@
 /**
- * HeaderSida 
+ * HeaderSida
  */
+
+import { Box, Grid } from '@mui/material';
+
 export default function HeaderSida() {
 	return (
 		<>
-			<h1>HeaderSida</h1>
+			<Grid container spacing={2}>
+				<Grid item={true} xs={12} sm={6} md={6} lg={5}>
+					<Box
+						sx={{
+							width: 300,
+							height: 300,
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							border: '1px solid gray',
+							overflow: 'hidden',
+						}}
+					>
+						<img
+							src="https://via.placeholder.com/300"
+							alt="example"
+							style={{
+								width: '100%',
+								height: '100%',
+								objectFit: 'cover', // Esto ajusta la imagen dentro del Box
+							}}
+						/>
+					</Box>
+				</Grid>
+			</Grid>
 		</>
 	);
 }
