a0e2b2747b1137712b83c44a721fe269188b0946
Andy <andycheung83a@hotmail.com>
2024-10-04 14:49:25 +0200

    Update image grid dimensions in HeaderSida component Grupp 1

--- frontend/src/components/views/HeaderSida.jsx
+++ frontend/src/components/views/HeaderSida.jsx
@@ -1,19 +1,26 @@
-/**
- * HeaderSida
- */
-
 import { Box, Grid } from '@mui/material';
 
+/**
+ 
+HeaderSida*/
 export default function HeaderSida() {
+	//<Grid item={true} xs={12} sm={6} md={6} lg={5}>
+
 	return (
 		<>
-			<Grid container spacing={2}>
-				<Grid item={true} xs={12} sm={6} md={6} lg={5}>
+			<Grid
+				container
+				spacing={10}
+				rowSpacing={1}
+				columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+			>
+				<Grid
+					container //style={{ minHeight: "80vh" }}
+				>
 					<Box
 						sx={{
-
 							width: '75rem',
-							height: "20rem",
+							height: '20rem',
 							display: 'flex',
 							justifyContent: 'center',
 							alignItems: 'center',
