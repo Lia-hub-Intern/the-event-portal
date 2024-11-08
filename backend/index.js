c24346ebce6b621b8761f1e810bea05f48db95f0
Pagona <peggykiriakogona@yahoo.com>
2024-11-08 17:29:45 +0100

    2024-11-08 justeringar för localhost

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,4 @@
-/**---------------------------------------------------------
- * Developer Full Stack: Darwin Rengifo
- *
- * Create Date: 2024-08-14
- *     Program : Stagefinder Index
- *   Path Name : stagefinder/index.js
- *       Tools : Javascript, NodeJs, Express, dotenv, nodemon
- *
- * Description:
- * - create express server.
- * - Cross-Origin Resource Sharing(CORS).
- *   Allows requests from domain-B to domain-A
- *   if domain-B is not configured, you will get a
- *   error for violation of CORS policy
- * - process.env.PORT || 8000.
- *   When the project is uploaded to the server,
- *   the server looks for an available port, if it
- *   does not find one, it assigns port 7000
- *-----------------------------------------------------------*/
+
 import express from "express"
 import dotenv from "dotenv"
 import routes from "./routes/routes.js"
@@ -28,10 +10,12 @@ import cors from "cors"
 dotenv.config()
 
 const app = express();
+//app.use(cors()) // har lagt det rad 13- kommenterat rad 18
 const PORT = process.env.PORT
 
+
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", " http://127.0.0.1:5173");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
