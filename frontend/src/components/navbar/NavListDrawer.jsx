a3d807d860929bed3cdd96173676d3c08d68e785
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2025-03-13 15:14:07 +0100

    Enhance NavListDrawer with animations and improved styling

--- frontend/src/components/navbar/NavListDrawer.jsx
+++ frontend/src/components/navbar/NavListDrawer.jsx
@@ -24,6 +24,8 @@ import {
   ListItemIcon,
   ListItemText,
   Typography,
+  Divider,
+  Fade
 } from "@mui/material";
 
 export default function NavListDrawer({ navBarLinks, NavLink, setOpen }) {
@@ -33,33 +35,56 @@ export default function NavListDrawer({ navBarLinks, NavLink, setOpen }) {
         <List>
           <ListItem sx={{ marginBottom: 3, paddingLeft: 3 }}>
             <ListItemText>
-              <Typography component="p" variant="p4">
+              <Typography component="p" variant="p4" sx={{ 
+                fontWeight: 700, 
+                color: 'primary.main',
+                borderBottom: '2px solid',
+                paddingBottom: '8px',
+                letterSpacing: '0.5px'
+              }}>
                 StageFinder
               </Typography>
             </ListItemText>
           </ListItem>
         </List>
+        <Divider sx={{ mb: 2 }} />
         <List>
-          {navBarLinks.map((item) => (
-            <ListItem sx={{ marginTop: -3 }} key={item.title}>
-              <ListItemButton
-                sx={{ paddingLeft: 0 }}
-                component={NavLink}
-                to={item.path}
-                onClick={() => setOpen(false)}
-              >
-                <ListItemIcon sx={{ minWidth: 0.2 }}>{item.icon}</ListItemIcon>
-                <ListItemText>
-                  <Typography
-                    component="p"
-                    variant="p5"
-                    sx={{ textTransform: "capitalize" }}
-                  >
-                    {item.title}
-                  </Typography>
-                </ListItemText>
-              </ListItemButton>
-            </ListItem>
+          {navBarLinks.map((item, index) => (
+            <Fade in={true} style={{ transitionDelay: `${index * 50}ms` }} key={item.title}>
+              <ListItem sx={{ marginTop: -1 }}>
+                <ListItemButton
+                  sx={{ 
+                    paddingLeft: 2,
+                    borderRadius: '8px',
+                    my: 0.5,
+                    '&:hover': {
+                      bgcolor: 'rgba(57, 73, 171, 0.08)',
+                      transform: 'translateX(5px)',
+                      transition: 'transform 0.2s ease-in-out'
+                    }
+                  }}
+                  component={NavLink}
+                  to={item.path}
+                  onClick={() => setOpen(false)}
+                >
+                  <ListItemIcon sx={{ minWidth: 40, color: 'primary.main' }}>
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText>
+                    <Typography
+                      component="p"
+                      variant="p5"
+                      sx={{ 
+                        textTransform: "capitalize",
+                        fontWeight: 500
+                      }}
+                    >
+                      {item.title}
+                    </Typography>
+                  </ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </Fade>
           ))}
         </List>
       </nav>
